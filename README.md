6b1cdac164d4ad159bc0831ec03143c7eac18b6a
PlagueCat-Miao <ace0413@126.com>
2020-11-05 20:24:54 +0800

    快速上传

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # movetojava
 形势所趋，喵喵喵喵
+## 编译&运行
+``` 
+    javac MoveToJava
+    java  MoveToJava
+``` 
+
+## 常量
+ - “ad”
+ - bool - false
---
+++ src/MoveToJava.java
@@ -0,0 +1,7 @@
+public class MoveToJava {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println(133);
+        System.out.println(false);
+    }
+}
